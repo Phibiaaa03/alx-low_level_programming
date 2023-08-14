278d78fc30b70caf698fd4174abc5cd003d55e40
Ololade <ololadeakintoye98@gmail.com>
2023-08-13 18:23:03 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function for this task
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
