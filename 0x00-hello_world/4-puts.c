311e89a68289913efb5fe7543d769b7884700a7a
Ololade <ololadeakintoye98@gmail.com>
2023-08-14 18:29:36 -0700

    multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with put function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
